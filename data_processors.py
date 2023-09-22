7a3e6007dff626e7629e44f3a3b4934d63ea4a06
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-09-22 19:13:50 +0100

    Feat: Use of regex to parse times. Unit tests are in order

--- data_processors.py
+++ data_processors.py
@@ -1,6 +1,29 @@
+from pandas import DataFrame
+
+
+def with_lengths(df: DataFrame) -> DataFrame:
+    df['length_m'] = df["length"].apply(convert_length)
+    return df.drop(columns=['length'])
+
 
 def convert_length(length: str) -> float:
     try:
         return float(str(length)[:-1])
     except:
         return 0.0
+
+
+def with_periods(df: DataFrame) -> DataFrame:
+    df[['epoch', 'period', 'mya_lower_bound', 'mya_upper_bound']] = df['period'].str.extract(
+        '(\w+)\s(\w+)\s(\d+)-(\d+)\smillion\syears\sago', expand=True
+    )
+    df['mya_lower_bound'] = df['mya_lower_bound'].apply(convert_mya)
+    df['mya_upper_bound'] = df['mya_upper_bound'].apply(convert_mya)
+    return df
+
+
+def convert_mya(mya: str) -> int:
+    try:
+        return int(mya)
+    except:
+        return 0
